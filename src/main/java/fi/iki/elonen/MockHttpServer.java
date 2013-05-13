e0bcf6165374f0b6e82987af9a364c810d1ea444
fernandohur <fernandohur@gmail.com>
2013-05-12 21:59:42 -0500

    added comments and how to use

--- src/main/java/fi/iki/elonen/MockHttpServer.java
+++ src/main/java/fi/iki/elonen/MockHttpServer.java
@@ -9,7 +9,24 @@ import main.java.fi.iki.elonen.NanoHTTPD.Response.Status;
 
 /**
  * The MockHttpServer can be used as a mock http server to test webservices
+ * The simplest way to use MockHttpServer is as follows:
+ * <code>
+ * //first start the server on your favourite port
+ * MockHttpServer server = new MockHttpServer(portNum);
+ * server.start();
  * 
+ * //We will add a mock response for every request we will do, so in this case, just one mock response
+ * server.enqueueResponse(Status.OK, "application/json,"{\"user_id\":15,\"name\":\"paul\"}");
+ * 
+ * //now we will use curl
+ * curl http://0.0.0.0:3000/users.json?name=paul
+ * 
+ * // we get the request object
+ * Request req = server.getRequest();
+ * assertEquals(req.getMethod(),"GET");
+ * assertEquals(req.getParams().get("name"),"paul")
+ * 
+ * </code>
  */
 public class MockHttpServer extends NanoHTTPD {
 
